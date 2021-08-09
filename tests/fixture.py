7911739b3eb6599bf3373a50c0b51b5b34c7715c
Esha Datta <edatta@crossref.org>
2021-08-09 19:44:41 -0400

    committing all non schema use cases for integration testing

--- tests/fixture.py
+++ tests/fixture.py
@@ -19,3 +19,7 @@ def invalid_links():
 @pytest.fixture(scope = "module")
 def invalid_relationship():
     return b"FOR FILE:  02baj6743\nVALIDATION TEST ERRORS: \n\nIn relationships: [{'https://ror.org/05qec5a53': ['Illegal relationship pairing: relationship type: Child should be Parent\']}, \"tests/fixtures/invalid/usecase-issues/015m7wh34.json doesn\'t exist\"]\n\n"
+
+@pytest.fixture(scope = "module")
+def invalid_iso_country_mismatch():
+    return b'FOR FILE:  02baj6749\nVALIDATION TEST ERRORS: \n\nIn check_address: {\'status\': {\'country_name\': {\'ror\': \'Fradnce\', \'geonames\': \'France\'}}}\n\nIn check_language_code: {\'status\': \'Language value: [{\\\'label\\\': "Centre d\\\'Investigation Clinique de Rennes", \\\'iso639\\\': \\\'ft\\\'}] is not an iso639 standard\'}\n\n'
--- tests/integration/test_invalid_usecases.py
+++ tests/integration/test_invalid_usecases.py
@@ -6,8 +6,11 @@ import copy
 from ..setup import *
 from ..fixture import *
 
+def invalid_fixture_path():
+    return "tests/fixtures/invalid/usecase-issues/"
+
 def invalid_files(key):
-    invalid = {"invalid_established_date": "tests/fixtures/invalid/usecase-issues/01k4yrm29-invalid-estdate.json", "invalid_links": "tests/fixtures/invalid/usecase-issues/03yrm5c26-invalid-link.json", "invalid_relationship": "tests/fixtures/invalid/usecase-issues/02baj6743.json"}
+    invalid = {"invalid_established_date": "01k4yrm29-invalid-estdate.json", "invalid_links": "03yrm5c26-invalid-link.json", "invalid_relationship": "02baj6743.json", "invalid_iso_country": "02baj6749.json"}
 
     return invalid[key]
 
@@ -15,14 +18,16 @@ def invalid_files(key):
 def test_invalid_established_date(invalid_established_date_address_mismatch):
     """This test sends an invalid file which passes the schema but has an invalid established date length."""
     file = invalid_files("invalid_established_date")
-    out, err, exitcode = run_args(file)
+    input = invalid_fixture_path()+file
+    out, err, exitcode = run_args(input)
     assert exitcode == 1
     assert out == invalid_established_date_address_mismatch
 
 def test_invalid_links(invalid_links):
     """This test sends an invalid file which passes the schema but has an invalid established date length."""
     file = invalid_files("invalid_links")
-    out, err, exitcode = run_args(file)
+    input = invalid_fixture_path()+file
+    out, err, exitcode = run_args(input)
     assert exitcode == 1
     assert out == invalid_links
 
@@ -30,6 +35,15 @@ def test_invalid_relshp(invalid_relationship):
     """This test sends an invalid file which passes the schema but has an invalid established date length."""
     file = invalid_files("invalid_relationship")
     o_args = run_optional_args(p = "tests/fixtures/invalid/usecase-issues")
-    out, err, exitcode = run_args(file, o_args)
+    input = invalid_fixture_path()+file
+    out, err, exitcode = run_args(input, o_args)
     assert exitcode == 1
     assert out == invalid_relationship
+
+def test_invalid_iso_country(invalid_iso_country_mismatch):
+    """This test sends an invalid file which passes the schema but has an invalid established date length."""
+    file = invalid_files("invalid_iso_country")
+    input = invalid_fixture_path()+file
+    out, err, exitcode = run_args(input)
+    assert exitcode == 1
+    assert out == invalid_iso_country_mismatch
