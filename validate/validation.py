ebcb9f256a9266049a92f662916793c37e7a5b9a
lizkrznarich <liz.krznarich@datacite.org>
2024-03-12 11:35:43 -0500

    add duplicate check to v2 links validation

--- validate/validation.py
+++ validate/validation.py
@@ -121,6 +121,9 @@ class Validate_Tests_V2:
         # removing empty strings
         links = list(filter(None, links))
         if len(links) > 0:
+            duplicates = vh.check_duplicates(links)
+        if len(duplicates) > 0:
+            msg['LINKS_DUPLICATES_ERROR'] = "Multiple links have the same value(s): " + ", ".join(duplicates)
             for l in links:
                 result = vh.validate_url(l)
                 if result:
